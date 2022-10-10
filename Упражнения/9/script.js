f8d5dd805559cf5022ba7fbe52e8c3e61bc6c871
KosaiToko <81806266+KosaiToko@users.noreply.github.com>
2022-10-10 10:46:04 +0500

    Add files via upload

---
+++ Упражнения/9/script.js
@@ -0,0 +1,17 @@
+/*
+$("#main-heading").text("Мои друзья");
+$("h1").fadeTo(2000, 0.5);
+var names = ["Кирилл", "Данил", "Артем"];
+for (var i = 0; i < names.length; i++) {
+    $("body").append("<p>" + names[i] + "</p>");
+    $("p").last().hide().fadeIn((i + 1) * 5000);
+}
+$("p").append(" лучший!");
+*/
+
+/*
+for (var i = 0; i<2;i++){
+    $("h1").slideUp(1000);
+    $("h1").slideDown(1000);
+}
+*/
